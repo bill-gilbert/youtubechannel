19f34ff645ace027b5c799c34f610ec3cede6c3e
Pavel Glukhov <pvglukhov@gmail.com>
2024-01-18 15:01:17 +0300

    added some changes

--- ss-c2/src/main/java/com/laurentiuspilca/springsecurityc2/controller/HelloController.java
+++ ss-c2/src/main/java/com/laurentiuspilca/springsecurityc2/controller/HelloController.java
@@ -1,13 +1,18 @@
 package com.laurentiuspilca.springsecurityc2.controller;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 public class HelloController {
 
     @GetMapping("/hello")
-    public String hello() {
+    public String hello(Authentication authentication) {
+        var u = authentication.getAuthorities();
+        u.forEach(a->log.info(a.getAuthority()));
         return "Hello!";
     }
 }
