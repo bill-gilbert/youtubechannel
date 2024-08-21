81deced828f4923c11f64296e146d7e43d5fa5a1
Pavel Glukhov <pvglukhov@gmail.com>
2024-08-21 17:53:37 +0300

    optimize imports

--- ss_2022_c2_e1/src/main/java/com/example/ss_2022_c2_e1/controllers/DemoController.java
+++ ss_2022_c2_e1/src/main/java/com/example/ss_2022_c2_e1/controllers/DemoController.java
@@ -1,6 +1,5 @@
 package com.example.ss_2022_c2_e1.controllers;
 
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
--- ss_2022_c2_e1/src/main/java/com/example/ss_2022_c2_e1/entities/Authority.java
+++ ss_2022_c2_e1/src/main/java/com/example/ss_2022_c2_e1/entities/Authority.java
@@ -3,7 +3,12 @@ package com.example.ss_2022_c2_e1.entities;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
 import java.util.Set;
 
 @Entity
